e86c4f35fdd4499a01df987d390d36b438ee1f68
ClementMonard <clement.monard@hotmail.fr>
2018-06-25 14:23:27 +0200

    Concaténation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Déclaration de la fonction switchImages qui prend en compte les paramètres id et image
-function switchImages(id, image) {
-  document.getElementById(id).src = image;
+function switchImages(img) {
+  document.getElementById(img).src = 'images/' + img + '_2.jpg';
+}
+
+function reverseImage (img) {
+  document.getElementById(img).src = 'images/' + img + '.jpg'
 }
